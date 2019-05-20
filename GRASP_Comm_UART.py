930aa57999e67c8ce57284f074edbb1dfac3f6b3
Kevin Huang <kevinh42@alumni.ubc.ca>
2019-05-20 00:50:21 -0600

    Trying out serial stuff
    I have to figure out how to actually test code.

--- GRASP_Comm_UART.py
+++ GRASP_Comm_UART.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python
 
 from GRASP_Comm import GRASP_Comm
+import serial
 
 class GRASP_Comm_UART(GRASP_Comm):
+    
+    ser = serial.Serial()
 
     def setup(self):
+        # Configure serial port
+        self.ser.port='/dev/serial0'
+        self.ser.baudrate=9600
+        self.ser.parity=serial.PARITY_NONE
+        self.ser.stopbits=serial.STOPBITS_ONE
+        self.ser.bytesize=serial.EIGHTBITS
+        self.ser.timeout=1
+        # Open serial port
+        self.ser.open()
+        super().setup()
         pass
 
     def receive_callback(self):
+        self.ser.read()
         super().receive_callback()
         pass
 
     def send(self, grip):
+        self.ser.write(grip)
         super().send(grip)
         pass
