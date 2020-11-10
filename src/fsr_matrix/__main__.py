34d2e02ad9d1bc361c35d92d8d437708fef61408
James <james.rosstwo@gmail.com>
2020-11-10 15:20:48 -0800

    add absolute pathing for file readers

--- src/fsr_matrix/__main__.py
+++ src/fsr_matrix/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 
 from src.definitions import ROOT_PATH
 from src.fsr_matrix.classifier.matrix_classifier import MatrixClassifier
@@ -9,6 +10,8 @@ from src.fsr_matrix.visualizer import MatrixVisualizer
 parser = argparse.ArgumentParser(description="Visualize FSR data in real time")
 parser.add_argument("--file", type=str, default=None,
                     help="Read from a file with a specified path relative to the root directory")
+parser.add_argument("--file_absolute_path", type=str, default=None,
+                    help="Read from a file with a specified absolute path")
 parser.add_argument("--port", type=str, default=None,
                     help="Read from a serial connection with a specified port")
 
@@ -16,6 +19,8 @@ args = parser.parse_args()
 
 if args.port is not None:
     _reader = UartReader(port=args.port)
+elif args.file_absolute_path is not None:
+    _reader = FileReader(Path(args.file_absolute_path))
 elif args.file is not None:
     _reader = FileReader(ROOT_PATH / args.file)
 else:
