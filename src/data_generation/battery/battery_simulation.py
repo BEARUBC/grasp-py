8a0eec8029762b2ff962bc1f4fcd8530fd4e7df6
Kira Li <kira.yw.li@gmail.com>
2021-08-24 16:41:56 -0700

    add influx to battery sim

--- src/data_generation/battery/battery_simulation.py
+++ src/data_generation/battery/battery_simulation.py
@@ -2,6 +2,8 @@ import random
 import time
 from typing import List, Generator
 
+from influxdb_client import InfluxDBClient, WriteOptions
+from datetime import datetime
 from src.data_generation.battery.battery_process import BatteryProcess
 
 
@@ -10,6 +12,11 @@ class BatterySimulation:
         self.processes = processes_list
         self.battery_life = 100
         self.buffertime = buffertime
+        # for influx:
+        self.url = "http://localhost:8086"
+        self.token = "Rbg3aKBu-nU_wY9wXkxCVLzT9WhH725mZ6LwEQgQjrppmeLYZ1J9xrjqXlZz6-oLfDJQhJWE169pyaN9rpmDzg=="
+        self.org = "0ed254cf3dca2b2b"
+        self.bucket = "GRASPDB"
 
     def reduce_battery(self):
         for process in self.processes:
@@ -26,6 +33,7 @@ class BatterySimulation:
                 else:
                     self.battery_life -= depletion
                     print("Battery = " + str(self.battery_life) + " (" + name + " -" + str(depletion) + ")")
+                self.influx_write(self.battery_life, datetime.now(), name)
 
     def run_simulation(self, real_time: bool = False, change_frequency: float = 0.40) -> Generator[int, None, None]:
         rand_cutoff = 100 * change_frequency
@@ -47,3 +55,16 @@ class BatterySimulation:
 
         yield self.battery_life
         print("Battery Depleted")
+
+    def influx_write(self, measurement, current_time, tag):
+        with InfluxDBClient(url=self.url, token=self.token, org=self.org) as _client:
+            # change write options params based on data batching
+            # see https://github.com/influxdata/influxdb-client-python#writes
+            with _client.write_api(write_options=WriteOptions(batch_size=500, flush_interval=10_000,
+                                                              jitter_interval=2_000, retry_interval=5_000,
+                                                              max_retries=5, max_retry_delay=30_000,
+                                                              exponential_base=2)) as _write_client:
+
+                _write_client.write(self.bucket, self.org,
+                                    {"measurement": "current battery level", "tags": {"current process": tag},
+                                     "fields": {"battery": measurement}, "time": current_time})
